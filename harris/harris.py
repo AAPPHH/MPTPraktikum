a802c1c70ac6f49a0afd43ce5da4f9f8a6bf89bf
Dennis Müller <mhulla1981@googlemail.com>
2025-05-12 17:22:47 +0200

    Started section about harris corner detection

---
+++ harris/harris.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+from scipy import signal
+
+def processImage(frame):
+    # Turn image into float32 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = np.float32(frame / 255.0)
+
+    # Calculate Gradient in X and Y, scale appropiately
+    gx = cv2.Sobel(frame, cv2.CV_32F, 1, 0, ksize=3) / 4.0
+    gy = cv2.Sobel(frame, cv2.CV_32F, 0, 1, ksize=3) / 4.0
+
+    # Calculate Ix^2, IxIy and Iy^2 images
+    gx2, gxgy, gy2 = gx ** 2, gx * gy, gy ** 2
+
+    # Apply filter
+    kernel = np.ones((7,7))
+    gx2 = signal.convolve2d(gx2, kernel) / 49
+    gy2 = signal.convolve2d(gy2, kernel) / 49 
+    gxgy = signal.convolve2d(gxgy, kernel) / 49
+    
+    
+    #gx2 = cv2.blur(gx2, ksize=(5,5))
+    #gy2 = cv2.blur(gy2, ksize=(5,5))
+    #gxgy = cv2.blur(gxgy, ksize=(5,5))
+
+    # Harris Corner Strength
+    kappa = 0.04
+    det = gx2 * gy2 - gxgy ** 2
+    trace = gx2 + gy2
+    strength = det - kappa *  trace**2
+    
+    strength = np.clip(strength, 0.0, 1.0)
+    strength /= np.max(strength) 
+
+    cv2.imshow("Harris Corner Strength", strength)
+
+
+def mainLoop():
+    """
+    The main loop of this program
+    """
+    # TODO: Open the default camera
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        # TODO: Read next image from camera
+        _, frame = cam.read()
+
+        # TODO: Call processImage to retrieve properly scaled gradient direction and magnitude images
+        processImage(frame)
+
+        # TODO: Also display the original camera image in color
+        cv2.imshow("Camera", frame)
+
+        # TODO: Break the infinite loop when the users presses ESCAPE (27)
+        if cv2.waitKey(1) == 27:
+            break
+
+    # TODO: Release the capture and writer objects
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    mainLoop()
